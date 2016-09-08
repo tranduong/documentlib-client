6e4e22e6a29de9f3ab1db5b8e92c5af5cf4d4ecf
Tran Duong <duongtb@fpt.edu.vn>
2016-09-08 08:31:00 +0200

    update readme

--- src/js/config/config.js
+++ src/js/config/config.js
@@ -3,5 +3,5 @@
 angular
     .module('SDLMSys')
 	.constant("DEPLOYED_HOST", {"URL": "http://localhost:3001"}) // Change when you have a new 
-	.constant("SEARCH_HOST", {"URL": "http://localhost:9200", "MULTI_API" : "_msearch", "SINGLE_API" : "_search"}) // Change when you have a new 
+	.constant("SEARCH_HOST", {"URL": "http://192.168.0.108:9200", "MULTI_API" : "_msearch", "SINGLE_API" : "_search"}) // Change when you have a new 
 	.constant("PAGINATION", {"ITEMS_PER_PAGE": 10});
